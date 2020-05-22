f3324a31714177b899168bdf9076b0efb7ee79b1
Kiëd Llaentenn <kiedtl@tilde.team>
2020-05-22 09:33:40 -0400

    display.c: fix display of colors

--- display.c
+++ display.c
@@ -9,7 +9,7 @@ display(struct CCMList *colors)
 {
 	for (struct CCMList *c = colors->next; c != NULL; c = c->next) {
 		printf(
-			"#%x%x%x\t\033[48;2;%i;%i;%im\033[K\033[0m\n",
+			"#%02x%02x%02x\t\033[48;2;%i;%i;%im\033[K\033[0m\n",
 			((struct Color *) c->data)->red,
 			((struct Color *) c->data)->green,
 			((struct Color *) c->data)->blue,
