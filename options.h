c14d21d25b553831a60641a8dd5df6ec94469013
Kiëd Llaentenn <kiedtl@tilde.team>
2020-05-23 12:33:08 -0400

    options.h: add

---
+++ options.h
@@ -0,0 +1,15 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include "bool.h"
+
+struct Options {
+	bool display_color;
+	bool display_hexrgb;
+
+	bool display_decrgb;
+	bool display_hsl;
+	bool display_hsv;
+};
+
+#endif
